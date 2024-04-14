5bee14683bb071e743d7f52bea13802fa0b5d3c9
Steven Hobs <47906512+stevenhobs@users.noreply.github.com>
2024-04-14 16:14:47 +0800

    fix

--- module/core.bat
+++ module/core.bat
@@ -1,8 +1,8 @@
-set USERPROFILE=%M_HOME%store
+set USERPROFILE=%MCODE_HOME%store
 set APPDATA=%USERPROFILE%\APPDATA\Roaming
 set LOCALDATA=%USERPROFILE%\APPDATA\Local
 set XDG_CONFIG_HOME=%USERPROFILE%\config
-set XDG_CACHE_HOME=%M_HOME%cache
+set XDG_CACHE_HOME=%MCODE_HOME%cache
 set TEMP=%XDG_CACHE_HOME%\temp
 set TMP=%TEMP%
 
@@ -11,14 +11,14 @@ if not exist "%LOCALDATA%" (mkdir %LOCALDATA%)
 if not exist "%XDG_CONFIG_HOME%" (mkdir %XDG_CONFIG_HOME%)
 if not exist "%TEMP%" (mkdir %TEMP%)
 
-set m=%M_HOME%sdk
+set m=%MCODE_HOME%sdk
 if exist "%m%\bin" set PATH=%m%\bin;%PATH%
 if exist "%m%\git\cmd" set PATH=%m%\git\cmd;%PATH%
 if exist "%m%\cmake\bin" set PATH=%m%\cmake\bin;%PATH%
 if exist "%m%\mingw64\bin" set PATH=%m%\mingw64\bin;%PATH%
 if exist "%m%\w64devkit\bin" set PATH=%m%\w64devkit\bin;%PATH%
 if exist "%m%\nvm\nvm.exe" (
-    set NVM_HOME=%m%\nvm
+    set NVMCODE_HOME=%m%\nvm
     set NVM_SYMLINK=%m%\nodejs
     set PATH=%m%\nodejs;%m%\nvm;%PATH%
 )
--- mterm.bat
+++ mterm.bat
@@ -3,10 +3,10 @@ setlocal
 REM PROXY
 set HTTP_PROXY=
 set HTTPS_PROXY=%HTTP_PROXY%
-if not "%M_HOME%" == "" goto exec_cmd
+if not "%MCODE_HOME%" == "" goto exec_cmd
 REM init
-set M_HOME=%~dp0
-call %M_HOME%\module\core.bat
-set PATH=%M_HOME%;%PATH%
+set MCODE_HOME=%~dp0
+call %MCODE_HOME%\module\core.bat
+set PATH=%MCODE_HOME%;%PATH%
 :exec_cmd
 cmd.exe /k prompt [MTERM]$S$E[92m$P$E[0m$S$$
