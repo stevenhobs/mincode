9621caa20dff84129b9b7099d52477f4d143b94e
Steven Hobs <47906512+stevenhobs@users.noreply.github.com>
2024-04-17 23:52:16 +0800

    fix code launch

--- mcode.bat
+++ mcode.bat
@@ -1,10 +1,12 @@
 @echo off
 setlocal
-set MCODE_HOME=%~dp0
-call %MCODE_HOME%\module\core.bat
-set PATH=%MCODE_HOME%;%PATH%
-set VSCODE_DEV=
-set ELECTRON_RUN_AS_NODE=1
-start /B "%~dp0ide\code\app\Code.exe" "%~dp0ide\code\app\resources\app\out\cli.js" %*
+if not "%MCODE_HOME%" == "" (goto open_code_directly) 
+set "MCODE_HOME=%~dp0"
+call "%MCODE_HOME%\module\core.bat"
+set "PATH=%MCODE_HOME%;%PATH%"
+:open_code_directly
+set "VSCODE_DEV="
+set "ELECTRON_RUN_AS_NODE=1"
+start /B %~dp0ide\code\app\Code.exe "%~dp0ide\code\app\resources\app\out\cli.js" %*
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 endlocal
